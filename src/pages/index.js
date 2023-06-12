73a0d151c8fa8edd31cf8e0ed6f479e26e67dcce
EmanuelDarioGarcia <pl5782100@gmail.com>
2023-06-12 15:39:47 -0300

    hero y carousel

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import Carousel from "comps/Carousel";
+import Hero from "comps/Hero";
+
+export default function Home() {
+    return (
+        <div>
+            <Hero />
+            <Carousel />
+        </div>
+    );
+}
