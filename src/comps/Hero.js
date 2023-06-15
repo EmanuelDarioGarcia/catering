c473f56ab31c2a7f4018ee28e55256dcd9fd5880
EmanuelDarioGarcia <pl5782100@gmail.com>
2023-06-14 22:40:26 -0300

    hero responsiveness fixed

--- src/comps/Hero.js
+++ src/comps/Hero.js
@@ -5,7 +5,7 @@ const Hero = ({ heading, message }) => {
         <div className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img">
             {/*overlay */}
             <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/70' />
-            <div className='p-5 text-black ml-[-10rem] mt-[-10rem]'>
+            <div className='p-5 text-black z-[2] mt-[-10rem]'>
                 <h2 className='text-5xl font-bold'> {heading} </h2>
                 <p className='py-5 text-3xl'> {message} </p>
                 <button className='px-8 py-2 border'>Contacto</button>
