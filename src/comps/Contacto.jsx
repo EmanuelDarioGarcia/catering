d6384bf9ec7b9a4aa06c5bdadecda94318b99004
EmanuelDarioGarcia <pl5782100@gmail.com>
2023-07-06 13:33:36 -0300

    z-index

--- src/comps/Contacto.jsx
+++ src/comps/Contacto.jsx
@@ -14,15 +14,15 @@ const Contacto = () => {
           <p className='basis-1/4 pl-4 text-xl '> 4280-1738</p>  
         </div>
         <div className='flex flex-row p-2'>
-          <FaPhone fill='#8b5ff2' size={30} className='z-10'/>
+          <FaPhone fill='#8b5ff2' size={30} className='z-9'/>
           <p className='basis-1/4 pl-4 text-xl '> 11-6440-4879</p>  
         </div>
         <div className='flex flex-row p-2'>
-          <FaFacebook fill='#8b5ff2' size={30} className='z-10' />
+          <FaFacebook fill='#8b5ff2' size={30} className='z-9' />
           <p className='basis-1/4 pl-4 text-xl '> Catering "La Turquesa"</p>  
         </div>
         <div className='flex flex-row p-2'>
-          <FaEnvelope  fill='#8b5ff2' size={30} className='z-10' />
+          <FaEnvelope  fill='#8b5ff2' size={30} className='z-9' />
           <p className='basis-1/4 pl-4 text-xl '> irmadul62@gmail.com</p>  
         </div>
       </div>
